9fbce9ea0645643832ed90bd6f65119f92c0e5fe
Chang Ming-Han <minghan@ming-hantekimacbook-pro.local>
2016-03-20 15:13:53 +0800

    Initial Commit

---
+++ enumm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  enumm
+//
+//  Created by Ming-Han Chang on 2016/3/20.
+//  Copyright © 2016年 Ming-Han Chang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
