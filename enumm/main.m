37d4e3a4f69da0f91a542682f85eaa63190e69a5
MingHan <sweetteaoh@gmail.com>
2016-07-20 13:35:55 +0800

    second commit

--- enumm/main.m
+++ enumm/main.m
@@ -10,8 +10,48 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        enum month : unsigned int { jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec};
+        enum month amonth;
+        int days;
+        
+        typedef enum {east, west, south, north } direction;
+        direction orientation1, orientation2;
+        typedef int counter;
+        counter value1, value2;
+
+        NSLog (@"Enter month number");
+        scanf ("%i", &amonth);
+        
+        switch (amonth)
+        {
+            case jan:
+            case mar:
+            case may:
+            case jul:
+            case aug:
+            case oct:
+            case dec:
+                days = 31;
+                break;
+            
+            case apr:
+            case jun:
+            case sep:
+            case nov:
+                days = 30;
+                break;
+            
+            case feb:
+                days = 28;
+                break;
+            }
+        
+        if (days != 0)
+            NSLog (@"num of days is %i", days);
+        
+
+            
     }
     return 0;
 }
